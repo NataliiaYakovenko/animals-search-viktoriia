47a55f7b49074a67e07d8766ef707a906b71bc1f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-24 21:09:00 +0300

    Add router to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
-
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import CreatePetPage from './pages/CreatePetPage/CreatePetPage';
+import PetsListPage from './pages/PetsListPage/PetsListPage';
 
 function App() {
   return (
-   <>
-   </>
+    <>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/pet/create">Add Pet</Link>
+            </li>
+            <li>
+              <Link to="/pets">Pets List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            HOME
+          </Route>
+          <Route path="./pet/create">
+            <CreatePetPage />
+          </Route>
+          <Route path="/pets">
+            <PetsListPage />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
